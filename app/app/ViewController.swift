88615da3817c8a794c36888f8bf40c41c74934fc
Óscar García Pérez <oscar.garcia@sngular.com>
2020-03-15 11:40:47 +0100

    project created

---
+++ app/app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Óscar García Pérez on 15/03/2020.
+//  Copyright © 2020 Sngular. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
